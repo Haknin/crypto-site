8ef8f197f0f25bf31535ab1ef2f3142bb62f50a7
‪Ori David Haknin <133655211+Haknin@users.noreply.github.com>
2023-07-19 23:23:54 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,13 @@
-#!/usr/bin/bash -xe
-curl -s $EC2_IP_TEST:5000
+#!/bin/bash
 
+# Check if the site is running by making a GET request to the homepage
+response=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:5000)
+
+# If the response code is 200, the site is running
+if [[ $response -eq 200 ]]; then
+    echo "Site is running"
+    exit 0  # Exit with success status
+else
+    echo "Site is not running"
+    exit 1  # Exit with failure status
+fi
